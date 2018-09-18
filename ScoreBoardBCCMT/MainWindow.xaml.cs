36d776d890c7a4d6dec1f6dddda860d359d826c6
TheerapakG <theerapakg@gmail.com>
2018-09-18 16:31:16 +0700

    Initial Commit

---
+++ ScoreBoardBCCMT/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace ScoreBoardBCCMT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Trace.WriteLine("new MainWindow");
+            InitializeComponent();
+        }
+
+        private void Button_PreviewMouseLeftButtonUp_Exit(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_PreviewMouseLeftButtonUp_Minimize(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+    }
+}
