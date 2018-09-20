6487a76744b2225c60f184b8b1fe927201616cb8
TheerapakG <theerapakg@gmail.com>
2018-09-20 09:59:55 +0700

    Prep for entries

---
+++ ScoreBoardBCCMT/EntriesLoader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows;
+
+namespace ScoreBoardBCCMT
+{
+    class EntriesLoader
+    {
+        /* Load entries information from list and push it into our list and listbox
+         * 
+        */
+        static void AppendEntries(ref ListBox listBox, List list)
+        {
+
+        }
+    }
+}
--- ScoreBoardBCCMT/MainWindow.xaml.cs
+++ ScoreBoardBCCMT/MainWindow.xaml.cs
@@ -21,10 +21,24 @@ namespace ScoreBoardBCCMT
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Entry> list = new List<Entry>();
+
         public MainWindow()
         {
             Trace.WriteLine("new MainWindow");
+            this.AddDynamicElements();
             InitializeComponent();
+        }
+
+        private void AddDynamicElements()
+        {
+            ListBox listBox = new ListBox();
+            Thickness listBoxthickness = new Thickness();
+            listBoxthickness.Left = 50;
+            listBoxthickness.Top = 170;
+            listBoxthickness.Right = 1465;
+            listBoxthickness.Bottom = 10;
+            listBox.Margin = listBoxthickness;
         }
 
         private void Button_PreviewMouseLeftButtonUp_Exit(object sender, MouseButtonEventArgs e)
